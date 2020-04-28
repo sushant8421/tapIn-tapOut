bbebc73de05dbb9956ea6f9908e5cef87264f101
Nishant Mor <nishant.mor@paytm.com>
2020-04-28 20:22:27 +0530

    clockInSubmitSelector: adding delay

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ async function click(selector, page) {
       })
       await page.click(selector)
     } catch (err) {
-    	console.log("click", err);
+    	console.log("click", err, selector);
     }
 }
 
@@ -28,7 +28,7 @@ async function type(selector, page, text){
 }
 
 async function hit() {
-
+	console.log("Hit Process")
 	const browser = await puppeteer.launch({
 		headless: true,
 		args: ['--no-sandbox']
@@ -43,17 +43,23 @@ async function hit() {
 	var passworSelector 		= '#UserLogin_password';
 	var loginButtonSelector 	= '#login-submit';
 	var dailySurveySelector 	= '#pulse_form > div > div > div > div.action-btns.mt-32 > button.btn.btn-secondary.ripple.db-btn.plr-32.mr-12.skip_pulse';
-	var profileSelector 		= "#dasboard-bigheader > header > div.col-md-4.text-right.mt-16.desktopDisplay > ul > li:nth-child(3) > div > div";
-	var clockInSelector 		= '#attendance-logger-widget'
+	//var profileSelector 		= "#dasboard-bigheader > header > div.col-md-4.text-right.mt-16.desktopDisplay > ul > li:nth-child(3) > div > div";
+	var clockInSelector 		= '#dasboard-bigheader > header > div.col-md-4.text-right.mt-16.desktopDisplay > ul > li:nth-child(1) > span'
 	var clockInSubmitSelector	= '#clokInClockout > div > div > div.modal-body > div.text-right > button'
 	await type(userNameSelector, page, USER_NAME);
 	await type(passworSelector, page, PASSWORD);
-	await click(loginButtonSelector, page);
-	await click(dailySurveySelector, page);
-	await click(clockInSelector, page);
-	await click(profileSelector, page);
-	await click(clockInSubmitSelector, page);
+	await new Promise(r => setTimeout(r, 10000)) // add wait ;
 
+	await click(loginButtonSelector, page);
+	await new Promise(r => setTimeout(r, 10000)) // add wait ;
+
+	await click(dailySurveySelector, page);
+	await new Promise(r => setTimeout(r, 10000)) // add wait ;
+
+	await click(clockInSelector, page);
+	await new Promise(r => setTimeout(r, 10000)) // add wait ;
+
+	await click(clockInSubmitSelector, page);
 	await browser.close();
 };
 
@@ -87,6 +93,9 @@ async function schedule1() {
 	}
 	else{
 		var t = Math.floor((Math.random() * 30) + 1)*60*1000;
+		if(process.env.NOW){ // Hit now 
+			t = 1;
+		}
 		setTimeout(function(){
 			hit();
 		}, t);
